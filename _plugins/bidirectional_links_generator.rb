8284df4243a10e038761683b6e5e5efcd2fdecfd
Eric Stein <steinea@protonmail.com>
2025-05-25 10:49:59 -0700

    Collapse plugin to attempt hook for pages and posts

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -1,23 +1,11 @@
 class BidirectionalLinksGenerator < Jekyll::Generator
   def generate(site)
 
-      Jekyll::Hooks.register :pages, :post_convert do |page|
+      Jekyll::Hooks.register([:posts, :pages], :post_convert) do |page|
+        all_posts = site.posts.docs
         all_pages = site.pages
 
-        all_docs = all_pages
-
-        all_docs.each do |current_note|
-          notes_linking_to_current_note = all_docs.filter do |e|
-            e.content.include?(current_note.url)
-          end
-          current_note.data['backlinks'] = notes_linking_to_current_note
-        end
-      end
-
-      Jekyll::Hooks.register :posts, :post_convert do |post|
-        all_posts = site.posts.docs
-
-        all_docs = all_posts
+        all_docs = all_posts + all_pages
 
         all_docs.each do |current_note|
           notes_linking_to_current_note = all_docs.filter do |e|
